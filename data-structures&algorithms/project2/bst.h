e38a3b7f5e16b62946a2c22ec5ac27548ddfccc0
Helen Yang <84105597+hyang373@users.noreply.github.com>
2023-03-19 16:51:00 -0400

    Create bst.h

---
+++ data-structures&algorithms/project2/bst.h
@@ -0,0 +1,40 @@
+/*
+ * File: bst.h
+ * Course: CS 315 - 001
+ * Project: Problem Set 2
+ * Purpose: Declaration of BST class
+ *      - Includes functions of a Binary Search Tree
+ */
+
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+using namespace std;
+
+class BST
+{
+        public:
+                // default constructor
+                create_tree();
+
+                // insert value into tree
+                void tree_insert(int value);
+
+                // delete a value from trree
+                void tree_delete(int value);
+
+                void transplant(int num1, int num2);
+
+                // find min of node pos
+                int tree_min(int pos) const;
+
+                // find max of node pos
+                int tree_man(int pos) const;
+
+                void tree_print() const;
+        private:
+                // declare private variable for binary search tree
+                vector<int> tree;
+};
+#endif /* BST_H */
